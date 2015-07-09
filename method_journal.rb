77b203eb0ea123508ec89c035b9d2d2546d8bf02
Colin Kelley <colindkelley@gmail.com>
2015-07-09 09:19:37 -0700

    Updates for SBonRails July 8, 2015

--- method_journal.rb
+++ method_journal.rb
@@ -1,36 +1,30 @@
 require 'active_support'
-require 'invoca/metrics'
 
-module PublishSuccessMetrics
-  include Invoca::Metrics::Source
+module MethodJournal
   extend ActiveSupport::Concern
 
 
   included do |base|
-    # Create a nested SuccessMetrics module and prepend it.
+    # Create a nested MethodJournalLayer module and prepend it.
     # This will be used to contain the wrapper methods.
     base.class_eval <<-EOS
-      module SuccessMetrics
+      module MethodJournalLayer
       end
 
-      prepend SuccessMetrics
+      prepend MethodJournalLayer
     EOS
   end
 
 
   module ClassMethods
-    def success_metric_prefix(metric_prefix)
-      @success_metric_prefix = metric_prefix
-    end
-
-    def publish_success_metric(method_name)
+    def journal_method(method_name)
       signature = method_signature(instance_method(method_name))
 
       # Define the wrapper method in the prepended module; we can then call the original method with `super`.
       # This is the Ruby 2 alternative to alias_method_chain.
-      self::SuccessMetrics.class_eval <<-EOS
+      self::MethodJournalLayer.class_eval <<-EOS
         def #{method_name}(#{signature})
-          publish_success_metric_wrapper(#{@success_metric_prefix.inspect}, #{method_name.inspect}) do
+          method_journal_wrapper(#{method_name.inspect}, #{signature}) do
             super
           end
         end
@@ -40,7 +34,7 @@ module PublishSuccessMetrics
     end
 
 
-  private
+    private
 
     def method_signature(method)
       method.parameters.map do |arg_type, arg_name|
@@ -48,17 +42,16 @@ module PublishSuccessMetrics
         arg_name.to_s
       end.join(', ')
     end
-
   end
 
-  def publish_success_metric_wrapper(*metric_names)
-    metric_name = metric_names.compact.join('.')
+  def method_journal_wrapper(method_name, *args)
     begin
+      puts "\n\n******** #{method_name}(#{args.inspect[1...-1]}) called ********"
       result = yield
-      metrics.increment("#{metric_name}.success")
+      puts "=======> returned #{result.inspect}\n\n"
       result
-    rescue
-      metrics.increment("#{metric_name}.failure")
+    rescue => ex
+      puts "=======> raised #{ex.class.name}: #{ex.message.inspect}\n\n"
       raise
     end
   end
--- script.rb
+++ script.rb
@@ -1,48 +1,125 @@
 require 'pry'; binding.pry
 
-module B
+class Base
   def ping
-    puts "B::test"
+    puts "Base::ping"
   end
 end
 
 module C
   def ping
-    puts "C::test"
+    puts "C::ping"
     super
   end
 end
 
 module D
   def ping
-    puts "D::test"
+    puts "D::ping"
+    super
+  end
+end
+
+
+class A < Base
+  include C
+  include D
+end
+
+>> A.ancestors
+
+#####################################
+
+
+class Base
+  def instance_method; puts "instance Base"; end
+  def self.class_method; puts "class Base"; end
+end
+
+class A < Base
+  def instance_method; puts "instance A\n"; super; end
+  def self.class_method; puts "class A\n"; super; end
+end
+
+a = A.new
+a.instance_method
+a.class.class_method
+
+#####################################
+
+module B
+  extend ActiveSupport::Concern
+  def instance_method; puts "instance B"; end
+  module ClassMethods
+    def class_method; puts "class B\n"; end
+  end
+end
+
+class A
+  include B
+  def instance_method; puts "instance A\n"; super; end
+  def self.class_method; puts "class A\n"; super; end
+end
+
+a = A.new
+a.instance_method
+a.class.class_method
+
+
+#####################################
+
+class Base
+  def ping
+    puts "Base::ping"
+  end
+end
+
+module C
+  def ping
+    puts "C::ping"
+    super
+  end
+end
+
+module D
+  def ping
+    puts "D::ping"
     super
   end
 end
 
 module E
   def ping
-    puts "E::test"
+    puts "E::ping"
     super
   end
 end
 
-class PartnerApi
-  include B
+module F
+  def ping
+    puts "F::ping"
+    super
+  end
+end
+
+class Api < Base
   include C
-  prepend D
+  include D
   prepend E
+  prepend F
 
   def ping
-    puts "PartnerApi::test"
+    puts "Api::ping"
     super
   end
 end
 
 ######################################
 
-p = PartnerApi.new
-p.ping
+A.ancestors
+
+a = Api.new
+a.ping
 
 ######################################
 
